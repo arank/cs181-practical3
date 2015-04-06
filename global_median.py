1066d4beb059dfeb9681b98389b36b7276602c40
Aran Khanna <arankhanna@college.harvard.edu>
2015-04-06 15:29:27 -0700

    added all reasonable files to repo

---
+++ global_median.py
@@ -0,0 +1,68 @@
+import numpy as np
+import csv
+import gzip
+
+# Predict via the median number of plays.
+profiles_file = 'profiles.csv.gz'
+artists_file = 'artists.csv.gz'
+train_file = 'train.csv.gz'
+test_file  = 'test.csv.gz'
+soln_file  = 'solutions.csv'
+
+
+user_profiles = {}
+def extract_user_data():
+    with gzip.open(profiles_file, 'r') as prof_fh:
+        prof_csv = csv.reader(prof_fh, delimiter=',', quotechar='"')
+        next(prof_csv, None)
+        for row in prof_csv:
+            user   = row[0]
+            sex = row[1] #yes please
+            age  = int(row[2])
+            location = row[3]
+            user_profiles[]
+
+def extract_artist_data():
+
+
+# Load the training data.
+train_data = {}
+with gzip.open(train_file, 'r') as train_fh:
+    train_csv = csv.reader(train_fh, delimiter=',', quotechar='"')
+    next(train_csv, None)
+    for row in train_csv:
+        user   = row[0]
+        artist = row[1]
+        plays  = int(row[2])
+    
+        if not user in train_data:
+            train_data[user] = {}
+        
+        train_data[user][artist] = plays
+
+# Compute the global median.
+plays_array = []
+for user, user_data in train_data.iteritems():
+    for artist, plays in user_data.iteritems():
+        plays_array.append(plays)
+global_median = np.median(np.array(plays_array))
+print "global median:", global_median
+
+# Write out test solutions.
+with gzip.open(test_file, 'r') as test_fh:
+    test_csv = csv.reader(test_fh, delimiter=',', quotechar='"')
+    next(test_csv, None)
+
+    with open(soln_file, 'w') as soln_fh:
+        soln_csv = csv.writer(soln_fh,
+                              delimiter=',',
+                              quotechar='"',
+                              quoting=csv.QUOTE_MINIMAL)
+        soln_csv.writerow(['Id', 'plays'])
+
+        for row in test_csv:
+            id     = row[0]
+            user   = row[1]
+            artist = row[2]
+
+            soln_csv.writerow([id, global_median])
